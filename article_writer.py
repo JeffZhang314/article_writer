bd631f9ed6385149df54d043646542cd32f69a15
JeffZhang314 <zhang.jj.jeff@gmail.com>
2026-05-13 13:49:12 -0700

    initial

---
+++ article_writer.py
@@ -0,0 +1,10 @@
+from google import genai
+
+client = genai.Client(api_key="AIzaSyBWnSRNLpXJR6NvcTwb1DPoDayg2qhPNY4")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="hello"
+)
+
+print(response.text)
