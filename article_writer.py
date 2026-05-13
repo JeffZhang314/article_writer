4c2bd1f24fc540bb7aa41fdc650bf0b36a8e070b
JeffZhang314 <zhang.jj.jeff@gmail.com>
2026-05-13 14:17:29 -0700

    yaml and secrets

--- article_writer.py
+++ article_writer.py
@@ -1,10 +1,12 @@
+import os
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBWnSRNLpXJR6NvcTwb1DPoDayg2qhPNY4")
+client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="hello"
-)
+#response = client.models.generate_content(
+#    model="gemini-2.5-flash",
+#    contents="hello"
+#)
 
-print(response.text)
+#print(response.text)
+print("test")
